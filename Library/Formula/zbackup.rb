318d45b08fd9511f6be681b8680b7ede72205392
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-03-27 21:09:24 +0200

    zbackup: temporary use maintainer's repo to update version to 1.3

--- Library/Formula/zbackup.rb
+++ Library/Formula/zbackup.rb
@@ -1,19 +1,25 @@
-require 'formula'
+require "formula"
 
 class Zbackup < Formula
-  homepage 'http://zbackup.org'
-  url 'https://github.com/zbackup/zbackup/archive/1.2.tar.gz'
-  sha1 'e87dfaeeeea0d59f4af00d3ce248aaabf1a25cb9'
+  homepage "http://zbackup.org"
+  url "https://github.com/eugenesan/zbackup.git", :commit => "1e8e941c6c0bb9377405dd78cd08f4d169564a81"
+  version "1.3"
+  head "https://github.com/eugenesan/zbackup,git", :branch => "master"
 
-  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
+  option "with-brewed-openssl", "Build with Homebrew OpenSSL instead of the system version"
 
-  depends_on 'cmake' => :build
-  depends_on 'openssl' if MacOS.version <= :leopard or build.with?('brewed-openssl')
-  depends_on 'protobuf'
-  depends_on 'xz' # get liblzma compression algorithm library from XZutils
+  depends_on "cmake" => :build
+  depends_on "openssl" if MacOS.version <= :leopard or build.with? "brewed-openssl"
+  depends_on "protobuf"
+  depends_on "xz"
 
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/zbackup", "--non-encrypted", "init", "."
+    system "echo test | #{bin}/zbackup backup backups/test.bak"
   end
 end
