5988e9a07ff188a61786f9f0c216e88fba7aab42
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-04-17 09:52:58 +0300

    radiant-player: update to 1.1.3

--- Library/Formula/radiant-player.rb
+++ Library/Formula/radiant-player.rb
@@ -2,15 +2,14 @@ require "formula"
 
 class RadiantPlayer < Formula
   homepage "https://github.com/kbhomes/google-music-mac"
-  url "https://github.com/kbhomes/google-music-mac/archive/v1.1.2.tar.gz"
-  sha1 "bc8da0beb26907319c6667338c6093f3d646951f"
+  url "https://github.com/kbhomes/google-music-mac/archive/v1.1.3.tar.gz"
+  sha1 "9c962efeaec17804b134865e3785322ebe37c9ee"
 
   depends_on :xcode
 
   def install
     # Fix CasE SeNsItIvItY!
-    File.rename "google-music-mac/LastFMService.h", "google-music-mac/LastFmService.h"
-    File.rename "google-music-mac/LastFMService.m", "google-music-mac/LastFmService.m"
+    inreplace "google-music-mac/LastFm/LastFmPopover.h", "LastFM", "LastFm"
 
     # Build embedded Pods project as dependency
     cd "Pods" do
@@ -19,7 +18,7 @@ class RadiantPlayer < Formula
 
     # Build main project
     xcodebuild "-configuration", "Release", "ONLY_ACTIVE_ARCH=YES", "SYMROOT=build"
-    bin.install 'build/Release/Radiant Player.app'
+    prefix.install 'build/Release/Radiant Player.app'
   end
 
   def caveats; <<-EOS
