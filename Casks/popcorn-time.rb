ae280a077efc3695cd77955cf69079fb411b0538
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-05-15 09:08:11 +0300

    Add popcorn-time

---
+++ Casks/popcorn-time.rb
@@ -0,0 +1,7 @@
+class PopcornTime < Cask
+  url 'http://cdn.get-popcorn.com/build/Popcorn-Time-0.3.0-Mac.tar.gz'
+  homepage 'http://get-popcorn.com/'
+  version '0.3.0'
+  sha256 '8f0f76fc3dccbccbfd433b7ca363c87ca6ab4515f3a3e39f6f08f402f654ae2e'
+  link 'Popcorn-Time.app'
+end
