c90192c6021ccf3d878ca051b69d8df4118641da
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-05-31 14:03:16 +0300

    mega-sdk: update to v2.0.0

--- Library/Formula/mega-sdk.rb
+++ Library/Formula/mega-sdk.rb
@@ -2,23 +2,29 @@ require 'formula'
 
 class MegaSdk < Formula
   homepage 'http://mega.co.nz'
-  url 'https://mega.co.nz/sdk.zip'
-  version '20131003'
-  sha1 'bd6aa846160631dfc4e8684000b41d087b36b46d'
+  url 'https://github.com/meganz/sdk2/archive/92d2fc2e4e.zip'
+  version '2.0.0'
+  sha1 'aa566b8e1774325c8a4c04808b1eea36d65a0427'
 
-  depends_on 'berkeley-db'
+  depends_on :autoconf
+  depends_on :automake
+  depends_on :libtool
+  depends_on 'sqlite3'
   depends_on 'cryptopp'
   depends_on 'readline'
   depends_on 'freeimage'
-  depends_on 'curl'
-
-  # Adapt for OSX
-  def patches; DATA end
+  depends_on 'c-ares'
+  depends_on 'doxygen'
+  depends_on 'curl' => [:build, 'with-ares']
 
   def install
-    system "make"
-    bin.install "megaclient"
-    doc.install "readme.txt"
+    system "./autogen.sh", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--enable-examples"
+    system "make", "doc", "install"
+    bin.install "examples/.libs/megacli"
+    bin.install "examples/.libs/megasync"
+    doc.install "README.md"
+    doc.install "doc/api/html"
   end
 
   def caveats; <<-EOS
@@ -31,23 +37,9 @@ class MegaSdk < Formula
        Including 3rParty/Indirect damages/claims!
     3. Each MEGA client application needs a valid key to access the API.
        Please use a separate key for each of your applications.
-       To do so visit https://mega.co.nz/#sdk
+       To do so visit https://mega.co.nz/#sdk or/and https://github.com/meganz/sdk2
 
     You must accept all above, otherwise, uninstall now!!!
     EOS
   end
 end
-
-__END__
---- a/Makefile
-+++ b/Makefile
-@@ -1,7 +1,7 @@
- CXX=g++
--CFLAGS=-g -Wall -isystem /usr/include/cryptopp
-+CFLAGS=-g -Wall -isystem /usr/local/include/cryptopp -D_DARWIN_C_SOURCE -I/usr/local/opt/readline/include
- LIB=-lcryptopp -lfreeimage -lreadline -ltermcap -lcurl -ldb_cxx
--LDFLAGS=
-+LDFLAGS=-L/usr/local/opt/readline/lib
- SOURCES=megaposix.cpp megacli.cpp megaclient.cpp megacrypto.cpp megabdb.cpp
- OBJECTS=$(SOURCES:.cpp=.o)
- PROGS=megaclient
