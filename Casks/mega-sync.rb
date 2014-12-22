bbf9d4f5ee0a60641d1d1f9b64b3f8451005378d
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-12-22 19:27:48 +0200

    mega-sync: rename and update cask to new syntax

--- Casks/mega-sync.rb
+++ Casks/mega-sync.rb
@@ -1,11 +1,11 @@
-class Megasync < Cask
-  version 'latest'
+cask :v1 => 'mega-sync' do
+  version :latest
   sha256 :no_check
 
   url 'https://mega.co.nz/MEGAsyncSetup.dmg'
   homepage 'https://mega.co.nz/#sync'
 
-  link 'MEGAsync.app', :target => '/Applications/MEGAsync.app'
+  app 'MEGAsync.app', :target => '/Applications/MEGAsync.app'
 
   caveats 'For security reasons, MEGAsync must be installed to /Applications and therefor is installed there.'
 end
