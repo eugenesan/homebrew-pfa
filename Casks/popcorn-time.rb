861fa67b1c0af8062e84099049768118e80839e0
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-12-22 19:28:27 +0200

    popcorn-tim: switch to latest version and update cask to new syntax

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -1,7 +1,10 @@
-class PopcornTime < Cask
-  url 'https://cdn.popcorntime.io/build/Popcorn-Time-0.3.3-Mac.dmg'
-  homepage 'http://popcorntime.io/'
-  version '0.3.3'
-  sha256 'd31246aea1222d9522c91ce71eebd0f1bbec570eaebd51b379a8b0d4f19a5f17'
-  link 'Popcorn-Time.app'
+cask :v1 => 'popcorn-time' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://popcorn-time.se/PopcornTime-latest.dmg'
+  homepage 'http://popcorn-time.se'
+  license :oss
+
+  app 'PopcornTime.app'
 end
